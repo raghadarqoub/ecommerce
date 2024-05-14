e7a298b5d0beebe2928b2dbddb2d30acb0a7d2ec
raghadarqoub <98125034+raghadarqoub@users.noreply.github.com>
2024-05-15 00:59:55 +0300

    Update app.router.js

--- src/app.router.js
+++ src/app.router.js
@@ -3,7 +3,7 @@ import categoriesRouter from './modules/category/category.router.js';
 import productRouter from './modules/product/product.router.js';
 
 const initApp =(app , express) => {
-    (connectDB);
+    connectDB();
     app.use(express.json());
     app.get('/', (req, res) => {
         return res.status(200).json({ message: "success" });
